cfc3bb8d184cbf762a1adddc7d2c18bdd6dfc8a5
Patrick Ohly <patrick.ohly@intel.com>
2018-06-05 10:54:58 +0200

    OIO driver test: run mount under pseudo
    This solves both "executable file not found" (for blkid, which is not
    in PATH of a normal user) and permission issues (the actual mount
    invocation) when running "go test" as normal user. The same code was
    also submitted to csi-test, see
    https://github.com/kubernetes-csi/csi-test/pull/75.
    
    One caveat remains: the OIO CSI driver currently needs read access to
    /dev/nbd*, otherwise it cannot determine which device nodes are
    unused. Adding the user to the "disks" group on Debian works around
    that.

--- pkg/oim-csi-driver/oim-driver_test.go
+++ pkg/oim-csi-driver/oim-driver_test.go
@@ -7,13 +7,52 @@ SPDX-License-Identifier: Apache-2.0
 package oimcsidriver
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/kubernetes-csi/csi-test/pkg/sanity"
 	"github.com/stretchr/testify/require"
 )
+
+// SudoMount provides wrappers around several commands used by the k8s
+// mount utility code. It then runs those commands under pseudo. This
+// allows building and running tests as normal users.
+type SudoMount struct {
+	tmpDir     string
+	searchPath string
+}
+
+func SetupSudoMount(t *testing.T) SudoMount {
+	tmpDir, err := ioutil.TempDir("", "sanity-node")
+	require.NoError(t, err)
+	s := SudoMount{
+		tmpDir:     tmpDir,
+		searchPath: os.Getenv("PATH"),
+	}
+	for _, cmd := range []string{"mount", "umount", "blkid", "fsck", "mkfs.ext2", "mkfs.ext3", "mkfs.ext4"} {
+		wrapper := filepath.Join(s.tmpDir, cmd)
+		content := fmt.Sprintf(`#!/bin/sh
+PATH=%q
+if [ $(id -u) != 0 ]; then
+   exec sudo %s "$@"
+else
+   exec %s "$@"
+fi
+`, s.searchPath, cmd, cmd)
+		err := ioutil.WriteFile(wrapper, []byte(content), 0777)
+		require.NoError(t, err)
+	}
+	os.Setenv("PATH", tmpDir+":"+s.searchPath)
+	return s
+}
+
+func (s SudoMount) Close() {
+	os.RemoveAll(s.tmpDir)
+	os.Setenv("PATH", s.searchPath)
+}
 
 func TestOIMDriver(t *testing.T) {
 	tmp, err := ioutil.TempDir("", "oim-driver")
@@ -24,6 +63,9 @@ func TestOIMDriver(t *testing.T) {
 	endpoint := "unix://" + tmp + "/oim-driver.sock"
 	s, err := driver.Start("oim-driver", "test-node", endpoint)
 	defer s.ForceStop()
+
+	sudo := SetupSudoMount(t)
+	defer sudo.Close()
 
 	// Now call the test suite.
 	config := sanity.Config{
