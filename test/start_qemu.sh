7721526a62cc021d8b007b03c575eb31daaabfa2
Patrick Ohly <patrick.ohly@intel.com>
2018-06-13 08:48:08 +0200

    start_qemu.sh: fixed boot order
    When starting the VM with additional disks we still need to boot
    from the real image.

--- test/start_qemu.sh
+++ test/start_qemu.sh
@@ -52,7 +52,8 @@ exec qemu-system-x86_64 \
     -smp sockets=1,cpus=4,cores=2 -cpu host \
     -m 1024 \
     -vga none -nographic \
-    -drive file="$IMAGE",if=virtio,aio=threads,format=raw \
+    -drive file="$IMAGE",if=none,aio=threads,format=raw,id=disk \
+    -device virtio-blk-pci,drive=disk,bootindex=0 \
     -netdev user,id=mynet0,hostfwd=tcp::${VMN}0022-:22,hostfwd=tcp::${VMN}2375-:2375 \
     -device virtio-net-pci,netdev=mynet0 \
     -debugcon file:debug.log -global isa-debugcon.iobase=0x402 \
